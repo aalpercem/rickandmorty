2d61c26dbe2bc4a2acaa464aac1cccc70ee8c8f6
Cem Ozturk <181906129@ogrenci.ibu.edu.tr>
2022-07-04 14:38:06 +0300

    Initial Commit

---
+++ rickandmorty/rickandmortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickandmortyApp.swift
+//  rickandmorty
+//
+//  Created by Cem Ozturk on 4.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct rickandmortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
